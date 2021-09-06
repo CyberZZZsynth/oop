fa12099639ec1835973179fcefc4855d7e418fed
Kirill <90137088+CyberZZZsynth@users.noreply.github.com>
2021-09-06 20:42:24 +0300

    Update main.cpp

--- oop_exercise_01/main.cpp
+++ oop_exercise_01/main.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-int mod(int a, int b) {
+int mod(int a, int b) { //создадим математическую функцию mod взятия остатка
     return (b+a%b)%b;
 }
 
-class angle{
+class angle{ //создадим класс углов angle
 
 private:
 
@@ -20,7 +20,7 @@ public:
     int grad;
     int minutes;
 
-    angle(int c, int d) {
+    angle(int c, int d) { //создадим конструктор класса angle
         grad = c+d/60;
         minutes = mod(d, 60);
         if(c > 0) {
@@ -28,47 +28,47 @@ public:
         }
     }
 
-    void printangle() {
+    void printangle() { //функция, которая выводит угол на экран пользователя
         cout << grad+minutes/60 << '*' << mod(minutes, 60) << "'" << '\n';
     }
 
-    void sum_angle(angle a, angle b) {
+    void sum_angle(angle a, angle b) { //функция, вычисляющая сумму углов
         rgrad = a.grad+b.grad+(a.minutes+b.minutes)/60;
         rminutes = mod((a.minutes+b.minutes), 60);
     }
 
-    void subt_angle(angle a, angle b) {
+    void subt_angle(angle a, angle b) { //функция, вычисляющая разность углов
         rgrad = a.grad-b.grad-(a.minutes-b.minutes)/60;
         rminutes = mod((a.minutes-b.minutes), 60);
     }
 
-    angle get_sum() {
+    angle get_sum() { //функция, возвращающая сумму углов
         angle result(rgrad, rminutes);
         return result;
     }
 
-    angle get_subt() {
+    angle get_subt() { //функция, возвращающая разность углов
         angle result(rgrad, rminutes);
         return result;
     }
 
-    double to_rad() {
+    double to_rad() { //функция, возвращающая величину угла в радианах
         rrad = grad*M_PI/180.0+minutes*M_PI/(60.0*180.0);
         return rrad;
     }
 
-    void to_360(angle a) {
+    void to_360(angle a) { //функция, приводящая угол к величине от 0 до 360 градусов
         int p=a.grad, q=a.minutes;
         rgrad = mod(p, 360);
         rminutes = mod(q, 60);
     }
 
-    angle get_360form() {
+    angle get_360form() { //функция, возвращающая величину угла от 0 до 360 градусов
         angle res(rgrad, rminutes);
         return res;
     }
 
-    bool is_equal(angle a, angle b) {
+    bool is_equal(angle a, angle b) {  //функция, сравнивающая 2 угла
         a.to_360(a);
         angle alpha = a.get_360form();
         b.to_360(b);
@@ -81,25 +81,25 @@ public:
         }
     }
 
-    float angle_cos(angle a) {
+    float angle_cos(angle a) { //функция, вычисляющая косинус угла и выводящая эту величину
         a.to_rad();
         double alpha = rrad;
         return cos(alpha);
     }
 
-    float angle_sin(angle a) {
+    float angle_sin(angle a) { //функция, вычисляющая синус угла и выводящая эту величину
         a.to_rad();
         double alpha = rrad;
         return sin(alpha);
     }
 
-    float angle_tan(angle a) {
+    float angle_tan(angle a) { //функция, вычисляющая тангенс угла и выводящая эту величину
         a.to_rad();
         double alpha = rrad;
         return tan(alpha);
     }
 
-    float angle_cot(angle a) {
+    float angle_cot(angle a) { //функция, вычисляющая котангенс угла и выводящая эту величину
         a.to_rad();
         double alpha = rrad;
         return 1/tan(alpha);
@@ -116,7 +116,7 @@ int main() {
     angle beta(c, d);
     alpha.printangle();
     beta.printangle();
-    cout << "Is alpha equal beta?" << '\t' <<  alpha.is_equal(alpha, beta)  << '\n';
+    cout << "Is alpha equal beta?" << '\n' <<  alpha.is_equal(alpha, beta)  << '\n';
     cout <<  "get alpha in rad:" << '\n' << alpha.to_rad() << '\n';
     cout <<  "get beta in rad:" << '\n' << beta.to_rad() << '\n';
     cout  << "alpha+beta is" <<  '\n';
