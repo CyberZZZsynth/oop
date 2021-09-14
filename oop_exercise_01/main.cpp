3fa22dcf3c37d86c8a1a3e24dfffa54b722a59da
Kirill <kirillwestcoast@gmail.com>
2021-09-14 23:07:37 +0300

    Update main.cpp

--- oop_exercise_01/main.cpp
+++ oop_exercise_01/main.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-int mod(int a, int b) { //создадим математическую функцию mod взятия остатка
+int mod(int a, int b) {
     return (b+a%b)%b;
 }
 
-class angle{ //создадим класс углов angle
+class angle{
 
 private:
 
@@ -20,7 +20,7 @@ public:
     int grad;
     int minutes;
 
-    angle(int c, int d) { //создадим конструктор класса angle
+    angle(int c, int d) {
         grad = c+d/60;
         minutes = mod(d, 60);
         if(c > 0) {
@@ -28,47 +28,47 @@ public:
         }
     }
 
-    void printangle() { //функция, которая выводит угол на экран пользователя
+    void printangle() {
         cout << grad+minutes/60 << '*' << mod(minutes, 60) << "'" << '\n';
     }
 
-    void sum_angle(angle a, angle b) { //функция, вычисляющая сумму углов
+    void sum_angle(angle a, angle b) {
         rgrad = a.grad+b.grad+(a.minutes+b.minutes)/60;
         rminutes = mod((a.minutes+b.minutes), 60);
     }
 
-    void subt_angle(angle a, angle b) { //функция, вычисляющая разность углов
+    void subt_angle(angle a, angle b) {
         rgrad = a.grad-b.grad-(a.minutes-b.minutes)/60;
         rminutes = mod((a.minutes-b.minutes), 60);
     }
 
-    angle get_sum() { //функция, возвращающая сумму углов
+    angle get_sum() {
         angle result(rgrad, rminutes);
         return result;
     }
 
-    angle get_subt() { //функция, возвращающая разность углов
+    angle get_subt() {
         angle result(rgrad, rminutes);
         return result;
     }
 
-    double to_rad() { //функция, возвращающая величину угла в радианах
+    double to_rad() {
         rrad = grad*M_PI/180.0+minutes*M_PI/(60.0*180.0);
         return rrad;
     }
 
-    void to_360(angle a) { //функция, приводящая угол к величине от 0 до 360 градусов
+    void to_360(angle a) {
         int p=a.grad, q=a.minutes;
         rgrad = mod(p, 360);
         rminutes = mod(q, 60);
     }
 
-    angle get_360form() { //функция, возвращающая величину угла от 0 до 360 градусов
+    angle get_360form() {
         angle res(rgrad, rminutes);
         return res;
     }
 
-    bool is_equal(angle a, angle b) {  //функция, сравнивающая 2 угла
+    bool is_equal(angle a, angle b) {
         a.to_360(a);
         angle alpha = a.get_360form();
         b.to_360(b);
@@ -81,55 +81,95 @@ public:
         }
     }
 
-    float angle_cos(angle a) { //функция, вычисляющая косинус угла и выводящая эту величину
+    float angle_cos(angle a) {
         a.to_rad();
         double alpha = rrad;
         return cos(alpha);
     }
 
-    float angle_sin(angle a) { //функция, вычисляющая синус угла и выводящая эту величину
+    float angle_sin(angle a) {
         a.to_rad();
         double alpha = rrad;
         return sin(alpha);
     }
 
-    float angle_tan(angle a) { //функция, вычисляющая тангенс угла и выводящая эту величину
+    float angle_tan(angle a) {
         a.to_rad();
         double alpha = rrad;
         return tan(alpha);
     }
 
-    float angle_cot(angle a) { //функция, вычисляющая котангенс угла и выводящая эту величину
+    float angle_cot(angle a) {
         a.to_rad();
         double alpha = rrad;
         return 1/tan(alpha);
     }
+
 };
 int main() {
     setlocale(LC_ALL, "Russian");
-    int a, b, c, d;
-    cout  << "Input angle alpha:" << '\n';
-    cin >> a >> b;
-    angle alpha(a,  b);
-    cout  << "Input angle beta:" << '\n';
-    cin >> c >> d;
-    angle beta(c, d);
-    alpha.printangle();
-    beta.printangle();
-    cout << "Is alpha equal beta?" << '\n' <<  alpha.is_equal(alpha, beta)  << '\n';
-    cout <<  "get alpha in rad:" << '\n' << alpha.to_rad() << '\n';
-    cout <<  "get beta in rad:" << '\n' << beta.to_rad() << '\n';
-    cout  << "alpha+beta is" <<  '\n';
-    alpha.sum_angle(alpha, beta);
-    (alpha.get_sum()).printangle();
-    cout << "alpha-beta is" << '\n';
-    alpha.subt_angle(alpha, beta);
-    (alpha.get_subt()).printangle();
-    cout << "alpha in 360* form:" << '\n';
-    alpha.to_360(alpha);
-    (alpha.get_360form()).printangle();
-    cout << "cos(alpha) = "  << alpha.angle_cos(alpha) << '\t' << "sin(alpha) = "  << alpha.angle_sin(alpha) << '\t' << "tg(alpha) = "
-         << alpha.angle_tan(alpha) << '\t' << "ctg(alpha) = "  << alpha.angle_cot(alpha) << '\n';
-    cout << "cos(beta) = "  << beta.angle_cos(beta) << '\t' << "sin(beta) = "  << beta.angle_sin(beta) << '\t' << "tg(beta) = "
-         << beta.angle_tan(beta) << '\t' << "ctg(beta) = "  << beta.angle_cot(beta) << '\n';
+    int c = 0;
+    angle alpha(0, 0);
+    while(1)
+    {
+        cout << "1. Input angle" << "\t" << "2. Print angle" << "\t" << "3. Print angle in radians" << "\t" << "4. Print angle in 360 form" << "\t" << "5. Sum and substraction"
+        << "\t" << "6. Compare angles" <<  "\t" << "7. Trigonometric functions" << "\t" << "8. Exit" << "\n";
+        scanf("%d", &c);
+        if (c==1)
+        {
+            int a, b;
+            cout << "Enter angle alpha" << '\n';
+            cin >> a >> b;
+            angle p(a,b);
+            alpha = p;
+        }
+        if (c==2)
+        {
+            alpha.printangle();
+        }
+        if (c==3)
+        {
+            cout << alpha.to_rad() << "\n";
+        }
+        if (c==4)
+        {
+            alpha.to_360(alpha);
+            alpha.get_360form();
+        }
+        if (c==5)
+        {
+            cout << "Enter angle beta" << "\n";
+            int c, d;
+            cin >> c >> d;
+            angle beta(c,d);
+            alpha.sum_angle(alpha, beta);
+            cout << "alpha+beta = ";
+            (alpha.get_sum()).printangle();
+            alpha.subt_angle(alpha, beta);
+            cout << '\t' << "alpha-beta = ";
+            (alpha.get_subt()).printangle();
+        }
+        if (c==6)
+        {
+            cout << "Enter angle beta" << "\n";
+            int c, d;
+            cin >> c >> d;
+            angle beta(c,d);
+            if(alpha.is_equal(alpha, beta)) {
+                cout << "alpha = beta";
+            } else {
+                cout << "alpha != beta";
+            }
+        }
+        if (c==7)
+        {
+            cout << "sin(alpha) = " << alpha.angle_sin(alpha) << "cos(alpha) = " << alpha.angle_cos(alpha) << "ctg(alpha) = " << alpha.angle_tan(alpha) <<
+            "ctg(alpha) = " << alpha.angle_cot(alpha) << '\n';
+        }
+        if (c==8)
+        {
+            break;
+        }
+    }
+    return 0;
 }
