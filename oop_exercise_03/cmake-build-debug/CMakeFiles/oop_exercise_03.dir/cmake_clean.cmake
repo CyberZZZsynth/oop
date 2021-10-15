c0d7fb8fc93bedf54e91acadb32acab2809fc15c
Kirill <kirillwestcoast@gmail.com>
2021-10-15 21:49:40 +0300

    Add Figure and Poinit classes

---
+++ oop_exercise_03/cmake-build-debug/CMakeFiles/oop_exercise_03.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_exercise_03.dir/Point.cpp.obj"
+  "CMakeFiles/oop_exercise_03.dir/main.cpp.obj"
+  "liboop_exercise_03.dll.a"
+  "oop_exercise_03.exe"
+  "oop_exercise_03.exe.manifest"
+  "oop_exercise_03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_exercise_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ oop_exercise_03/cmake-build-debug/Figure.h
@@ -0,0 +1,21 @@
+//
+// Created by Кирилл on 14.10.2021.
+//
+
+#ifndef OOP_EXERCISE_03_FIGURE_H=
+#define OOP_EXERCISE_03_FIGURE_H
+
+#include "Point.h"
+
+class Figure {
+public:
+    virtual size_t get_Vertex() = 0;
+    virtual double get_Area()=0;
+    virtual void Print(std::ostream &out) = 0;
+    ~Figure() {
+        std::cout << "Figure deleted!" <<  '\n';
+    };
+};
+
+
+#endif //OOP_EXERCISE_03_FIGURE_H
