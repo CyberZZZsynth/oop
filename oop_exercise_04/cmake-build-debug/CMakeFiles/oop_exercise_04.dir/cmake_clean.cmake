74abd9d3c6c3ce5fe2ef657f6282b68698913af2
Kirill <kirillwestcoast@gmail.com>
2021-10-29 21:17:48 +0300

    add lab4

---
+++ oop_exercise_04/cmake-build-debug/CMakeFiles/oop_exercise_04.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_exercise_04.dir/main.cpp.obj"
+  "liboop_exercise_04.dll.a"
+  "oop_exercise_04.exe"
+  "oop_exercise_04.exe.manifest"
+  "oop_exercise_04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_exercise_04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
